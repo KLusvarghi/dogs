af02b96a5eabd2053a7798832e4dc7e01bed944a
Kauã Ortolani Lusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2022-10-13 17:28:35 -0300

    criação dos componentes do formulário

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
     // essa é a url para fazer ogin, que na verdade estará puxando o token do usuário para autenticação
 
     fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
@@ -15,32 +17,25 @@ const LoginForm = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({username,password}),
-    }).then(response => {
-      console.log(response)
-      return response.json()
-    }).then(json => {
-      console.log(json)
-      return json
+      body: JSON.stringify({ username, password }),
     })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" value={username} name="username" />
+        <Input label="Senha" type="password" value={password} name="password" />
+        <Button>Entrar</Button>
       </form>
 
       <Link to="/login/criar">Cadastro</Link>
