91e1e5f5d22577a9ff62fce4b45f8893e6fa4382
Kauã Ortolani Lusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2022-10-14 09:05:23 -0300

    UseContext feito

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST, USER_GET } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginForm = () => {
   const username = useForm();
@@ -13,44 +12,17 @@ const LoginForm = () => {
   // console.log(username.value)
   // console.log(password.value)
 
-  // Caso ao site montar na tela já tenha um token do usuário no localSorage, ele já valida para mim
-
-  useEffect(() => {
-    // puxando o toquem do localstorage se houver
-    const getToken = window.localStorage.getItem('token')
-    if(getToken){
-      getUser(getToken)
-    }
-  }, [])
-
-
-  async function getUser(token) { // função para puzar o Usuário
-    const {url, options} = USER_GET(token)
-    const response = await fetch(url, options)
-    const json = await response.json()
-    console.log(json)
-  }
-
+  // já desestruturando o meu userLogin
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    if (username.validate() && password.validate()) { // assim ele tendo que validar os inputs antes de fazer o fech e validar o usuário
-
-      // desestruturando o que vem de TOKEN_POST
-      const {url, options} = TOKEN_POST({ // vindo minha api e options de TOKEN_POST
-        username: username.value,
-        password: password.value,
-      })
-
-
-      const response = await fetch(url, options)
-      const json = await response.json()
-      //console.log(json) // me retornando o toke do usuário
-
-      // Passando o token para o localstorage
-      window.localStorage.setItem('token', json.token)
-      getUser(json.token)
+    if (username.validate() && password.validate()) {
+      // assim ele tendo que validar os inputs antes de fazer o fech e validar o usuário
+      
+      // usando o userContex, simplificando as coisas
+      userLogin(username.value, password.value)
     }
   }
 
