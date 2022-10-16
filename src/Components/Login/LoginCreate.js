e0c08cbe5ed6deb15f323c199cbd2569d23e4ba1
Kauã Ortolani Lusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2022-10-16 19:15:22 -0300

    Hook useFetch concluido

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
-import useForm from '../../Hooks/useForm';
+import Error from '../Helper/Error';
 import { USER_POST } from '../../api';
 import UserContext from '../../UserContext';
-
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
 
 const LoginCreate = () => {
-  const username = useForm() // para fazer validação por tipo de input, porem nesse caso não tem nenhum tipo
-  const email = useForm('email')
-  const password = useForm()
+  const username = useForm(); // para fazer validação por tipo de input, porem nesse caso não tem nenhum tipo
+  const email = useForm('email');
+  const password = useForm();
 
   // usando o contexto para obeter a função de fazer login
-  const {userLogin} = React.useContext(UserContext)
+  const { userLogin } = React.useContext(UserContext);
 
-  
+  const { loading, error, request } = useFetch(); // não sendo necessario pegar o 'data' porque não é necessario do data na criação do usuário
+
   // email, usuario, username e senha para a criação de um novo usuário
   async function handleSubmit(event) {
     event.preventDefault();
-    const {url, options} = USER_POST({
+    const { url, options } = USER_POST({
       // tendo que passar o valor dos input's
       username: username.value,
       email: email.value,
       password: password.value,
-    })
-    const response = await fetch(url, options)
-    if(response.ok){ // só faço o login do usuário caso a resposta seja 'ok'
-      userLogin(username.value, password.value) // chamando a função de fazer login passando o usename e senha
-
+    });
+    const { response } = await request(url, options); //usando a função criado no 'useFetch'
+    if (response.ok) {
+      // só faço o login do usuário caso a resposta seja 'ok'
+      userLogin(username.value, password.value); // chamando a função de fazer login passando o usename e senha
     }
-
-
-
 
     // console.log(username.value) // retorna os dados como id, username, name e email do usuário
     // console.log(response) // me retorna se foi criado com exito ou não
@@ -44,7 +43,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="Email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+// como o site utiliza o fetch da mesma maneira diversas vezes, foi criado esse hook para facilitar o trabalho
+
+
+const useFetch = () => {
+  // tendo no fetch o data que segura os dados
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  // criando uma função de request, criando ela dentro de um callback para que ela não seja recriada toda vez que a gente utilizar o useFetch
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null; // passando null para que na hora de setar o data, o processo não seja demorado passando json do fetch sendo que o 'response.ok' é false
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []); // passando uma array vazia por que não há dependencias
+
+  return {
+    data,
+    error,
+    loading,
+    request,
+  };
+};
+
+export default useFetch;
