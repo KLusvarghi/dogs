2e451eeeee553b323a1b344745ceeaf1fb840978
Kauã Ortolani Lusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2022-10-16 17:47:52 -0300

    estilização final da página de login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { Route, Routes, Navigate } from 'react-router-dom'
-import LoginForm from './LoginForm'
-import LoginCreate from './LoginCreate'
-import LoginPasswordLost from './LoginPasswordLost'
-import LoginPasswordReset from './LoginPasswordReset'
-import { UserContext} from '../../UserContext'
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   // para desativar ou sumir da tela o formulário de login caso já esteja logado, usamos o estado (login) do mu useContext
-  const {login} = React.useContext(UserContext)
-  // fazedno um direcionamento de rota porem dessa vez não sendo com o useNavigate e sim com um elemento do 
-  if(login === true) return <Navigate to="/conta" />
+  const { login } = React.useContext(UserContext);
+  // fazedno um direcionamento de rota porem dessa vez não sendo com o useNavigate e sim com um elemento do
+  if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm/>} />
-        <Route path='criar' element={<LoginCreate/>} />
-        <Route path='perdeu' element={<LoginPasswordLost/>} />
-        <Route path='resetar' element={<LoginPasswordReset/>} />
-      </Routes>
-    </div>
-  )
-}
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
